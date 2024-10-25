6e1b84e2b5932c27e63490a2d83883e34de37b15
Alessio <83132320+Alessiop01@users.noreply.github.com>
2024-10-25 21:34:54 +0200

    Update port in main.py

--- src/forensicWace_SE/main.py
+++ src/forensicWace_SE/main.py
@@ -596,7 +596,7 @@ def DiscoverMore():
 
 def main():
     """Defines the main function of the application."""
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=3000, debug=True)
 
 
 if __name__ == '__main__':
